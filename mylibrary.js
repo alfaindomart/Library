265a23804e56d64ce4abc0aee0f324a946acaf3c
alfaindomart <alfaindomart23@gmail.com>
2023-09-23 21:16:21 +0700

    add function to remove book from library array

--- mylibrary.js
+++ mylibrary.js
@@ -7,6 +7,7 @@
 //a card shows up. Shows the inputted book form data
 
 const myLibrary = [];
+let bookIndex = 0
 
 //handle the form dialog
 const bookDialog = document.getElementById('bookDialog'); 
@@ -19,25 +20,37 @@ showForm.addEventListener('click', () => {
   bookDialog.showModal()
 });
 
+
 function book (title, author, pages, isRead) {
     this.title = title
     this.author = author
     this.pages = pages
     this.isRead = isRead
-    this.pushBook = function () {
-      myLibrary.push(this)
-    }
-    this.pushBook();
+    this.index = bookIndex
+    bookIndex++
+    // this.pushBook = function () {
+    //   myLibrary.push(this)
+    // }
+    // this.pushBook();
 };
 
-addBook.addEventListener('click', (e) => {
-
+function addBookToLibrary () {
   let bookTitle = document.getElementById('bookTitle').value;
   let author = document.getElementById('authors').value;
   let pages = document.getElementById('pages').value;
   let isRead = document.getElementById('haveRead').checked;
 
-  e.preventDefault();
+  let newBook = new book(bookTitle, author, pages, isRead);
+  myLibrary.push(newBook);
+}
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+}
+
+function renderBook() {
+
+  for (const obj of myLibrary) {
 
   let cards = document.createElement('div');
   let cardTitle = document.createElement('h2');
@@ -46,41 +59,92 @@ addBook.addEventListener('click', (e) => {
   let cardReadStat = document.createElement('div');
   let cardRemoveBtn = document.createElement('button');
 
+  cards.classList.add('cards');
+  cardTitle.classList.add('cd-title');
+  cardAuthor.classList.add('cd-author');
+  cardPages.classList.add('cd-pages');
+  cardReadStat.classList.add('cd-read-stat');
+  cardRemoveBtn.classList.add('cd-remove');
+
   cardWrap.appendChild(cards);
-cards.append(cardTitle, cardAuthor, cardPages, cardReadStat, cardRemoveBtn);
+  cards.append(cardTitle, cardAuthor, cardPages, cardReadStat, cardRemoveBtn);
 
-cards.classList.add('cards');
-cardTitle.classList.add('cd-title');
-cardAuthor.classList.add('cd-author');
-cardPages.classList.add('cd-pages');
-cardReadStat.classList.add('cd-read-stat');
-cardRemoveBtn.classList.add('cd-remove');
-
-  let newBook = new book(bookTitle, author, pages, isRead);
-  for (const prop in newBook) {
-    switch (prop) {
-      case 'title':
-        cardTitle.textContent = newBook[prop];
-        break;
-      case 'author':
-        cardAuthor.textContent = newBook[prop];
-        break;
-      case 'pages':
-        cardPages.textContent = newBook[prop];
-        break;
-      default:
-        // Handle unknown properties if needed
-        break;
-    }
-    if (!isRead) {
-      cardReadStat.textContent = "not read"
-    } else {cardReadStat.textContent = "Read";
-  };
-  cardRemoveBtn.innerHTML = "remove";
-  bookDialog.close();
-    }
+  cardTitle.textContent = obj.title;
+  cardAuthor.textContent = obj.author;
+  cardPages.textContent = obj.pages;
+  cardRemoveBtn.addEventListener('click', (e) => {
+      removeBook(obj.index)
+    })
+  //   if (!isRead) {
+  //     cardReadStat.textContent = "not read"
+  //   } else {cardReadStat.textContent = "Read";};
   }
-)
+};
+
+addBook.addEventListener('click', (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+  renderBook();
+  bookDialog.close();
+})
+
+
+  
+
+
+
+
+// addBook.addEventListener('click', (e) => {
+
+//   let bookTitle = document.getElementById('bookTitle').value;
+//   let author = document.getElementById('authors').value;
+//   let pages = document.getElementById('pages').value;
+//   let isRead = document.getElementById('haveRead').checked;
+
+//   e.preventDefault();
+
+//   let cards = document.createElement('div');
+//   let cardTitle = document.createElement('h2');
+//   let cardAuthor = document.createElement('p');
+//   let cardPages = document.createElement('p');
+//   let cardReadStat = document.createElement('div');
+//   let cardRemoveBtn = document.createElement('button');
+
+//   cardWrap.appendChild(cards);
+// cards.append(cardTitle, cardAuthor, cardPages, cardReadStat, cardRemoveBtn);
+
+// cards.classList.add('cards');
+// cardTitle.classList.add('cd-title');
+// cardAuthor.classList.add('cd-author');
+// cardPages.classList.add('cd-pages');
+// cardReadStat.classList.add('cd-read-stat');
+// cardRemoveBtn.classList.add('cd-remove');
+
+//   let newBook = new book(bookTitle, author, pages, isRead);
+//   for (const prop in newBook) {
+//     switch (prop) {
+//       case 'title':
+//         cardTitle.textContent = newBook[prop];
+//         break;
+//       case 'author':
+//         cardAuthor.textContent = newBook[prop];
+//         break;
+//       case 'pages':
+//         cardPages.textContent = newBook[prop];
+//         break;
+//       default:
+//         // Handle unknown properties if needed
+//         break;
+//     }
+//     if (!isRead) {
+//       cardReadStat.textContent = "not read"
+//     } else {cardReadStat.textContent = "Read";
+//   };
+//   cardRemoveBtn.innerHTML = "remove";
+//   bookDialog.close();
+//     }
+//   }
+// )
 
 let cardWrap = document.getElementById('cards-wrapper');
 
